ded8bd192ef74f931d88bb6a833286c203961b0f
李超 <lichao@ahxz.vip>
2023-11-25 12:43:29 +0800

    fix: 修改通知配置，使支持群名白名单为空

--- szy/main.py
+++ szy/main.py
@@ -13,7 +13,7 @@ from szy.wechat_notice import WechatNotice
 class SzyServices:
     def __init__(self):
         msgKeywordWhiteList = conf().get("szy_msg_keyword_white_list", ['【三只羊供应链】'])
-        nameWhiteList = conf().get("szy_msg_name_white_list", ['~三只羊通知'])
+        nameWhiteList = conf().get("szy_msg_name_white_list", [])
 
         self.notice = WechatNotice(msgKeywordWhiteList, nameWhiteList)
         self.mq_worker = MqWorker(wechatNotice=self.notice)
